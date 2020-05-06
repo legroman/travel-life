db654acd84fc2966d33768b7f0c9895d3ee21230
Python-Development <legromant@gmail.com>
2020-05-06 18:29:58 +0300

    Slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import "materialize-css/dist/css/materialize.min.css";
 import './App.css';
 import Header from "./components/Header";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Slider/>
     </div>
   );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, {useEffect} from "react";
+import slide0 from "../img/slide-0.jpg";
+import slide1 from "../img/slide-1.jpg";
+import slide2 from "../img/slide-2.png";
+import slide3 from "../img/slide-4.jpg";
+import slide4 from "../img/slide-3.jpg";
+
 
 export default function Slider() {
-    return(
-        <section>
+    useEffect(() => {
+        const elems = document.querySelectorAll('.slider');
+        window.M.Slider.init(elems, {});
+    }, []);
 
+    return (
+        <section className="slider">
+            <ul className="slides">
+                <li>
+                    <img src={slide0} alt="slide0"/>
+                    <div className="caption center-align">
+                        <h3>This is our big Tagline!</h3>
+                        <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src={slide1} alt="slide1"/>
+                    <div className="caption left-align">
+                        <h3>Left Aligned Caption</h3>
+                        <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src={slide2} alt="slide2"/>
+                        <div className="caption right-align">
+                            <h3>Right Aligned Caption</h3>
+                            <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                        </div>
+                </li>
+                <li>
+                    <img src={slide3} alt="slide3"/>
+                        <div className="caption center-align">
+                            <h3>This is our big Tagline!</h3>
+                            <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                        </div>
+                </li>
+                <li>
+                    <img src={slide4} alt="slide4"/>
+                    <div className="caption left-align">
+                        <h3>Left Aligned Caption</h3>
+                        <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    </div>
+                </li>
+            </ul>
         </section>
     )
 }
